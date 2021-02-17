56ff836c2d0d42f131d02abf606dbb34f61fedb6
thebadams <bpeteradams@gmail.com>
2021-02-16 19:52:18 -0500

    Week 3 Activity 6

--- week-3/03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
+++ week-3/03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -1,16 +1,25 @@
+// declares a variable, called 'one' = 64 -> number
 var one = 64;
+// declares a variable, called 'two' = "656302" -> string
 var two = "656302";
+
 var three = false;
 var four = 64.55;
 var five = "Howdy!";
 var six;
 
 // Insert comments to explain what each console log below will log to the console
+// console logs number
 console.log(typeof one); 
+// console logs string
 console.log(typeof two); 
+// console logs boolean
 console.log(typeof three); 
+// console logs number
 console.log(typeof four); 
+// console logs string
 console.log(typeof five);
+// console logs undefined
 console.log(typeof six);
 
 four = "Hello!";
@@ -18,6 +27,9 @@ five = false;
 six = 23;
 
 // Insert comments to explain what each console log below will log to the console
+// console logs string
 console.log(typeof four);
+// console logs boolean
 console.log(typeof five);
+// console logs number
 console.log(typeof six);
