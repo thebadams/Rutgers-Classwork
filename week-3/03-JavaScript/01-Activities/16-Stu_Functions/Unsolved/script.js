9e4d69c7c100f7995f9a1c8a3d7a57bc08cf3298
thebadams <bpeteradams@gmail.com>
2021-02-18 19:53:29 -0500

    Week 3 Activity 16 Functions

--- week-3/03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
+++ week-3/03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
@@ -1,1 +1,11 @@
 // Write Your Code Below
+function testEquality(x,y) {
+    if(x===y){
+        console.log("The values are equal in type and value");
+    } else if (x==y) {
+        console.log("The values are equal in value");
+    } else {
+        console.log("The values are not equal");
+    }
+    return;
+}
