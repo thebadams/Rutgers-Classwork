cf9148bacf146c44a7c6b37ea3c1a45043655543
thebadams <bpeteradams@gmail.com>
2021-02-18 19:03:32 -0500

    Week 3 activity 12, Student Arrays

--- week-3/03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ week-3/03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,17 @@
 // WRITE YOUR CODE HERE
+var classroom = ["Aakash", "Alderick", "Aldo", "Ashley"];
+console.log(classroom.length);
+
+console.log (`Welcome to Class ${classroom[0]}`);
+
+console.log(`Welcome to Class ${classroom[1]}`)
+
+console.log(`Welcome to Class ${classroom[2]}`)
+
+console.log(`Welcome to Class ${classroom[3]}`)
+
+classroom[0] = "Benjamin"
+
+if(classroom[0]== "Benjamin") {
+    console.log(`${classroom[0]} is in class`)
+}
