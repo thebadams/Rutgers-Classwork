b0e24a03f82c0bec6505af2ec0ab5e34bb9796a3
thebadams <bpeteradams@gmail.com>
2021-02-23 21:37:23 -0500

    Week 4 Activity 8 Create and Append

--- week-4/04-Web-APIs/01-Activities/08-Stu_Create-Append/Unsolved/script.js
+++ week-4/04-Web-APIs/01-Activities/08-Stu_Create-Append/Unsolved/script.js
@@ -36,3 +36,9 @@ kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
 favoriteEl.setAttribute("style", "font-size:20px;");
 
 // TODO: Add ordered list items containing four favorite foods
+
+let favoriteFoods = ["Fish", "Bird", "Mouse", "Fruits"];
+listEl.appendChild(li1).textContent = favoriteFoods[0];
+listEl.appendChild(li2).textContent = favoriteFoods[1];
+listEl.appendChild(li3).textContent = favoriteFoods[2];
+listEl.appendChild(li4).textContent = favoriteFoods[3];
