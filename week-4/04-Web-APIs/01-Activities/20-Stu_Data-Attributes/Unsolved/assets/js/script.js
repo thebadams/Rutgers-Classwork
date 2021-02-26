3303105db36b235c897cd0b72094c508c24eb701
thebadams <bpeteradams@gmail.com>
2021-02-25 21:22:13 -0500

    activities, 2/25/21

--- week-4/04-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ week-4/04-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -4,4 +4,18 @@ container.addEventListener("click", function(event) {
   var element = event.target;
 
   // TODO: Complete function
+  if(element.matches("div")) {
+    var state = element.getAttribute("data-state");
+    var number = element.getAttribute("data-number");
+    if(state === "hidden") {
+      element.dataset.state = "revealed";
+      element.setAttribute("data-state", "revealed");
+      element.textContent = number;
+      
+    } else {
+      element.dataset.state = "hidden";
+      element.setAttribute("data-state", "hidden");
+      element.textContent = ""
+    }
+  }
 });
