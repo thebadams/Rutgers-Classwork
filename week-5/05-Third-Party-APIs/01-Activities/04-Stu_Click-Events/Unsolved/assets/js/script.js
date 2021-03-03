cb7a9f25bcded0bc46a8b8d715e32701fd0e8c24
thebadams <bpeteradams@gmail.com>
2021-03-02 22:11:33 -0500

    class activities 2.27.21

--- week-5/05-Third-Party-APIs/01-Activities/04-Stu_Click-Events/Unsolved/assets/js/script.js
+++ week-5/05-Third-Party-APIs/01-Activities/04-Stu_Click-Events/Unsolved/assets/js/script.js
@@ -1,4 +1,4 @@
-var passwordBtnEl = $('.password-btn');
+var passwordBtnEl = $('#password-btn');
 var passwordDisplayEl = $('#password-display');
 
 // Returns a random character that includes alphanumeric and special character values
@@ -15,7 +15,7 @@ function passwordGenerator(num) {
   return password;
 }
 
-passwordBtnEl.on('dblclick', function () {
+passwordBtnEl.on('click', function () {
   var newPassword = passwordGenerator(15);
   passwordDisplayEl.text(newPassword);
 });
--- week-5/05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ week-5/05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -2,5 +2,12 @@ var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function getSubmission(e){
+    e.preventDefault();
+    var shoppingItem = $('#shopping-input');
+    shoppingListEl.append(`<li>${shoppingItem.val()}</li>`)
+    shoppingItem.val('');
+}
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on('submit', getSubmission);
