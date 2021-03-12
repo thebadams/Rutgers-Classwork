1fd35f615eeae87664d3dd9aebaa33fe107b75a9
Brian Adams <bpeteradams@gmail.com>
2021-03-11 21:34:08 -0500

    thursdays activities - fetch requests

--- week-6/06-Server-Side-APIs/01-Activities/18-Stu__Fetch_Options/Unsolved/assets/js/script.js
+++ week-6/06-Server-Side-APIs/01-Activities/18-Stu__Fetch_Options/Unsolved/assets/js/script.js
@@ -1,4 +1,13 @@
-fetch('https://api.github.com/repos/nodejs/node/issues?per_page=5')
+var requestUrl = 'https://api.github.com/repos/nodejs/node/issues?per_page=5'
+
+var fetchOptions = {
+  method: 'GET',
+  credentials: 'same-origin',
+  cache: 'reload',
+  redirect: 'follow'
+}
+
+fetch(requestUrl, )
   .then(function (response) {
     return response.json();
   })
